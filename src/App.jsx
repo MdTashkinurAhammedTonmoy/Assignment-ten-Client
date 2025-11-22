1574bf42e18f7200eeff084163e1493437a550f3
MdTashkinurAhammedTonmo <md11tonmoy1613@gmail.com>
2025-11-22 11:28:31 +0600

    Assigement start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>ami jani na je amar ki kora uchis.ami sob dik diye pic pore jachi ami ekhon ki korbo</h1>
+      
+    </>
+  )
+}
+
+export default App
